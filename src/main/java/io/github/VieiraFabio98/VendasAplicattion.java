ec050e12ab77a56a9432f9dd5dd22050564a310a
FabioAlvesVA <fabio.alves@vamilly.com.br>
2023-08-04 14:38:24 -0300

    Iniciando a aplicação Spring Boot.

---
+++ src/main/java/io/github/VieiraFabio98/VendasAplicattion.java
@@ -0,0 +1,12 @@
+package io.github.VieiraFabio98;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasAplicattion {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendasAplicattion.class, args);
+    }
+}
