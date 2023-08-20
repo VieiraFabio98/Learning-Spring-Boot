4786f032ffe3c8eae267b5b00b0ff487e5071b90
VieiraFabio98 <vieirafabio5271@outlook.com>
2023-08-20 12:01:32 -0300

    Injeção de dependências.

---
+++ src/main/java/io/github/VieiraFabio98/repository/ClientesRepository.java
@@ -0,0 +1,12 @@
+package io.github.VieiraFabio98.repository;
+
+import io.github.VieiraFabio98.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+
+    //acessr base e salvar cliente
+    public void persistir(Cliente cliente) {
+    }
+}
